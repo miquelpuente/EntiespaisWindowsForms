1a5f4ce7e97996d92b27a32ed08e2ac9d51dbc11
Dani <danielmejialban@gmail.com>
2019-04-08 19:24:19 +0200

    Creación de ADO.NET

---
+++ EntiEspais-adri/EntiEspais/Proyecto/BaseDatos/BD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntiEspais.Proyecto
+{
+   public static  class BD
+    {
+
+
+    }
+}
