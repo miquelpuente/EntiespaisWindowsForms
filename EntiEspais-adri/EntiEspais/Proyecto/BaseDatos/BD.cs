5fd734cff99c8c89e9cc865ab0cdb2b04697166e
Dano <danielmejialban@gmail.com>
2019-04-09 19:47:37 +0200

    Borrar Instalación

--- EntiEspais-adri/EntiEspais/Proyecto/BaseDatos/BD.cs
+++ EntiEspais-adri/EntiEspais/Proyecto/BaseDatos/BD.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,91 +19,6 @@ namespace EntiEspais.Proyecto
         public static void cargarEntidades(BindingSource binding) {
             binding.DataSource = BaseDatos.Entidades.selectAllEntidades();
         }
-
-
-        /// <summary>
-        /// Método el cuál nos ayuda a conocer nuestra Excepción con un mensaje, esto nos obliga a que nuestros métodos que no devolvian nada 
-        /// ahora devuelvan un String con el mensaje correspondiente a la Excepción.
-        /// </summary>
-        /// <param name="sqlex"></param>
-        /// <returns></returns>
-        public static String mensajesExceptions(SqlException sqlex)
-        {
-
-            string mensaje;
-            switch (sqlex.Number)
-            {
-                case -1:
-                    mensaje = "Error de conexión con el servidor";
-                    break;
-                case 547:
-                    mensaje = "Tienes datos relacionados";
-                    break;
-                case 2601:
-                    mensaje = "Datos duplicados";
-                    break;
-                case 2627:
-                    mensaje = "Datos duplicados";
-                    break;
-                case 4060:
-                    mensaje = "No se encuentra la base de datos";
-                    break;
-                case 18456:
-                    mensaje = "Usuario o contraseña incorrectos";
-                    break;
-                default:
-                    mensaje = sqlex.Number + " - " + sqlex.Message;
-                    break;
-            }
-            return mensaje;
-        }
-
-        /// <summary>
-        /// Este método hace un "revert" de los cambios que hayamos podido realizar a la hora de ejecutar cualquier instrucción CRUD.
-        /// </summary>
-        public static void rejectChanges()
-        {
-
-            foreach (DbEntityEntry entry in bd.ChangeTracker.Entries())
-            {
-
-                switch (entry.State)
-                {
-                    case System.Data.Entity.EntityState.Detached:
-                        break;
-                    case System.Data.Entity.EntityState.Unchanged:
-                        break;
-                    case System.Data.Entity.EntityState.Added:
-                        break;
-                    case System.Data.Entity.EntityState.Deleted:
-                        break;
-                    case System.Data.Entity.EntityState.Modified:
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Guarda los cambios que hayamos podido realizar en la BD llamando así mismo a la funcion SaveChanges().
-        /// </summary>
-        /// <returns></returns>
-        public static String SaveChanges()
-        {
-
-            String mensaje = "";
-            try
-            {
-                BD.bd.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                rejectChanges();
-                SqlException sqlexc = (SqlException)ex.InnerException.InnerException;
-                mensaje = mensajesExceptions(sqlexc);
-            }
-            return mensaje;
-        }
+        
     }
 }
--- EntiEspais-adri/EntiEspais/Proyecto/BaseDatos/Instalaciones.cs
+++ EntiEspais-adri/EntiEspais/Proyecto/BaseDatos/Instalaciones.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntiEspais.Proyecto.BaseDatos
-{    
+{
    public static class Instalaciones
     {
-        private static String mensaje = "";
 
         public static List<Instal_lacio>selectAllInstalaciones(){
             List<Instal_lacio> listaInstalaciones =
@@ -20,18 +17,5 @@ namespace EntiEspais.Proyecto.BaseDatos
                 ).ToList();
             return listaInstalaciones;
         }
-
-        /// <summary>
-        /// Borramos una instalación y contralamos un posible error pasando este mismo para poder saber de que exception de SQL se trata.
-        /// </summary>
-        /// <param name="hotel"></param>
-        /// <returns></returns>
-        public static String deleteInstalacion(Instal_lacio instal_Lacio) {
-            BD.bd.Instal_lacio.Remove(instal_Lacio);
-            mensaje = BD.SaveChanges();
-            return mensaje;
-
-        }
     }
-
 }
